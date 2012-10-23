64a23cb50e8339d8a08aafa929c0506477e69d9d
La rosa kévin <larosa.kevin@gmail.com>
2012-10-23 23:17:52 +0200

    Définition de la structure sous backbones, implémentation du loader & vidéo avec controle navigateur

--- src/modules/models.js
+++ src/modules/models.js
@@ -1,7 +1,31 @@
-// Déclaration du model
-app.Models.personnage = Backbone.Model.extend();
-
-// Déclaration de la collection
-app.Collections.personnages = Backbone.Collection.extend({
-    model: app.Models.personnage
+app.Models.User = Backbone.Model.extend({
+	
+		defaults: {
+			userAgent:'?',
+			version:'?',
+			videoWatch: false,
+			gameStart:false,
+			etape:[
+				{id:1,unlock:false,point:0},
+				{id:2,unlock:false,point:0},
+				{id:3,unlock:false,point:0},
+				{id:4,unlock:false,point:0},
+				{id:5,unlock:false,point:0},
+				{id:6,unlock:false,point:0},
+				{id:7,unlock:false,point:0},
+				{id:8,unlock:false,point:0},
+				{id:9,unlock:false,point:0},
+				{id:10,unlock:false,point:0},
+			],
+			totalPoint:0
+		},
+		
+		initialize : function User(){
+			console.log("Modèle de l'utilisateur initialisé");
+			this.bind("error", function(model, error){
+        		console.log( error );
+        		console.log(model);
+    		});
+		}
+			
 });
--- src/modules/views.js
+++ src/modules/views.js
@@ -1,25 +1,110 @@
-app.Views.main = Backbone.View.extend({
-  el: '#content',
-  // Fonction appelé automatiquement lors de l'instanciation de la vue
-  initialize: function () {
-    // déclaration des templates
-    this.templateMenu = $('#templateMenu').html();
-    this.templatePerso = $('#templatePerso').html();
-    // on initialise le menu
-    this.initMenu();
-  },
-  initMenu: function () {
-    // On transforme notre collection en un tableau de Hash
-    var personnages = app.collections.personnages.toJSON();
-    // Création des éléments du menu en utilisant le template et nos personnages
-    var menuHTML = _.template(this.templateMenu, { personnages: personnages });
-    // On affiche le menu
-    this.$('#menu').html(menuHTML);
-  },
-  displayPerso: function (perso) {
-    // On génère le html pour afficher la fiche du personnage
-    var persoHTML = _.template(this.templatePerso, { perso: perso });
-    // On l'affiche
-    this.$('#content-perso').html(persoHTML)
-  }
+/**
+ * View du loader
+ * @author Kévin La Rosa
+ * @requires  backbones.js
+ */
+app.Views.loader = Backbone.View.extend({
+	el : '#loader',
+	// Fonction appelé automatiquement lors de l'instanciation de la vue
+	initialize : function() {
+		this.templateLoader  = $('#templateLoader').html()
+		//Déclaration du noeud html de destination
+		this.noeud
+
+		//Lancement du rendu de chargement
+		this.LoaderRender();
+
+	},
+	// Chargements des ressources Videos & sons & images
+	LoaderRender : function() {
+		//Définition des ressources (Définir les images)
+		app.Assets.images.background = app.loader.addImage('assets/img/headerbg.jpg'), app.Assets.images.treesImg = app.loader.addImage('assets/img/trees.png'), app.Assets.images.ufoImg = app.loader.addImage('assets/img/ufo.png');
+		
+		//Chargement du bon fichier video ATTTENTION IL FAUT FINIR EN AJOUTER LES AUTRES FORMAT PAR NAVIGATEURS
+		if (Modernizr.video) {
+		  if (Modernizr.video.webm) {
+		    app.Assets.videos.intro = app.loader.addVideo('assets/video/intro.mp4', 'movie', 10);
+		  } else if (Modernizr.video.ogg) {
+		    app.Assets.videos.intro = app.loader.addVideo('assets/video/intro.mp4', 'movie', 10);
+		  } else if (Modernizr.video.h264){
+		    app.Assets.videos.intro = app.loader.addVideo('assets/video/intro.mp4', 'movie', 10);
+		  }
+		}else{
+			//proposer du flash !!
+		}
+		
+		//Référence vers mon template de chargement
+		app.loader.templateLoader = this.templateLoader;
+		// On prépare notre noeud HTML a partir de son template
+		loaderHTML = underscore.template(this.templateLoader,{avancement:""});
+		// On l'affiche
+		$('#loader').html(loaderHTML);
+		
+		//Ecoute et répond jusqu'au moment ou toutes les ressources sont chargés
+		app.loader.addCompletionListener(function() {
+			//supprime le loader
+			$('#loader').remove();
+		});
+		
+		//Ecoute et répond a chaque avancement du chargement
+		app.loader.addProgressListener(function(e) {
+			templateLoader  = $('#templateLoader').html()
+			progression = e.completedCount+'/'+e.totalCount;
+			loaderHTML = underscore.template(app.loader.templateLoader,{avancement:progression});
+			console.log('Chargement des assets en cours  : '+ progression);
+			
+		});
+		
+		//Lance le loader
+		app.loader.start();
+	},
 });
+
+
+/**
+ * View de l'accueil
+ * @author Kévin La Rosa
+ * @requires  backbones.js
+ */
+app.Views.home = Backbone.View.extend({
+	el : '#Accueil',
+	// Fonction appelé automatiquement lors de l'instanciation de la vue
+	initialize : function() {
+		// Déclaration des templates
+		this.templateAccueil = $('#templateAccueil').html();
+		
+		// Déclaration de référance (Demander a pumir si il existe mieux ~~)
+		var renderAccueil = this.renderAccueil;
+		
+		//On lance la vidéo si l'utilisateur n'a jamais vu celle-ci (localstorage a faire)
+		if(true){
+			this.loaderVideo(renderAccueil);
+			//this.renderAccueil();
+			
+		}
+		
+		
+	},
+
+	loaderVideo : function(renderAccueil) {
+		//$('#videoIntro').html(app.Assets.videos.intro);
+		//app.Assets.videos.intro.play();
+		renderAccueil();
+		app.Assets.videos.intro.addEventListener('ended',function(){
+		 	//jQuery('#videoIntro > video ').hide('clip'); 
+		 	//$('#videoIntro').remove();
+		 	renderAccueil();
+		})
+		
+	},
+	
+	renderAccueil: function(){
+		a = underscore.template(this.templateAccueil,{});
+		console.log(this.templateAccueil);
+		$('#Accueil').html('accueilHTML');
+		console.log(accueilHTML);
+		
+
+	}
+	
+}); 
