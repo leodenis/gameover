5d96e2bf01da871256e7b6b12009bcfae2d98074
Mathieu DUTTO <dutto.mathieu@gmail.com>
2012-12-03 20:54:15 +0100

    modifs css

--- src/modules/views.js
+++ src/modules/views.js
@@ -866,10 +866,10 @@ app.Views.etape3 = app.Views.question.extend({
 app.Views.etape4 = app.Views.question.extend({
 	render : function (){
 		//Définition des variables à passer
-		video1 = {'url':'23608259','titre':'Un chat mais pas que..','description':''};
-		video2 = {'url':'5169262','titre':'Un perroquet mais pas que...','description':''};
+		video1 = {'url':'23608259','titre':'Un chat.. peu commun !','description':''};
+		video2 = {'url':'5169262','titre':'Un perroquet.. atypique !','description':''};
 		//Recupère le html générer avec le template
-		template = accueilHTML = _.template($('#templateVideos').html(),{'titreQuestion':'La solitude peut tuer ! Choisissez-vous un animal de compagnie qui vous remontera le moral.','video1':video1,'video2':video2});
+		template = accueilHTML = _.template($('#templateVideos').html(),{'titreQuestion':'Choisissez un animal de compagnie qui vous remontera le moral.','video1':video1,'video2':video2});
 		this.$el.html(template);
 		
 		app.Helpers.videosVimeo();
@@ -887,7 +887,7 @@ app.Views.etape5 = app.Views.question.extend({
 		video1 = {'url':'35132562','titre':'Will Smith - I am a Legend','description':''};
 		video2 = {'url':'3431743','titre':'Chuck Norris','description':''};
 		//Recupère le html générer avec le template
-		template = accueilHTML = _.template($('#templateVideos').html(),{'titreQuestion':'Les conseils de quelle star préféreriez-vous entendre pour vous préparer à la fin du monde ?','video1':video1,'video2':video2});
+		template = accueilHTML = _.template($('#templateVideos').html(),{'titreQuestion':'Quels conseils écouter pour vous préparer à la fin du monde ?','video1':video1,'video2':video2});
 		this.$el.html(template);
 		
 		app.Helpers.videosVimeo();
@@ -903,7 +903,7 @@ app.Views.etape6 = app.Views.question.extend({
 	
 	render: function(){
 		//Recupère le html générer avec le template
-		accueilHTML = _.template($('#templateStreetView').html(),{'titreQuestion':'Vous semblez fatigué. Trouvez donc un endroit pour passer la nuit. Vous pouvez choisir entre un Bunker, un Hôtel 5* ou l’arche la plus proche de votre position.'});
+		accueilHTML = _.template($('#templateStreetView').html(),{'titreQuestion':'Trouvez un endroit pour passer la nuit : un bunker, un hôtel ou l\'arche la plus proche ?'});
 		this.$el.html(accueilHTML);
 		//Définition des paramètre de la street + map (voir helper)
 		var optionModeStreetMap = {
@@ -1066,7 +1066,7 @@ app.Views.etape7 = app.Views.question.extend({
 		image2 = {'url':'ipad3.png','alt':'cut','titre':'Couteau','description':'Choisissez le couteau utile et perène !!!'};
 
 		//Recupère le html générer avec le template
-		template = accueilHTML = _.template($('#templateWebGl').html(),{'titreQuestion':'BLA BLA ?','image1':image1,'image2':image2});
+		template = accueilHTML = _.template($('#templateWebGl').html(),{'titreQuestion':'Ces cauchemars donnent envie de se protéger mais que choisir ?','image1':image1,'image2':image2});
 		this.$el.html(template);
 
 		var camera_gun, camera_cut, scene_gun, scene_cut, renderer_gun, renderer_cut;
@@ -1225,7 +1225,7 @@ app.Views.etape8 = app.Views.question.extend({
 		//Fil ariane
 		app.Helpers.filAriane(app.Helpers.getLastQuestUnlock(),app.Helpers.getCurrentQuestion());
 		//Recupère le html générer avec le template
-		template = _.template($('#templateStreetView').html(),{"titreQuestion":"Entendez-vous ? Je crois que l’heure à sonner... Choisissez la direction dans laquelle vous souhaitez partir."});
+		template = _.template($('#templateStreetView').html(),{"titreQuestion":"Vous entendez ? L\'heure a sonné, choisissez une direction.."});
 		this.$el.html(template);		
 		// Définition des paramètre de la street + map (voir helper)
 		var optionModeStreetMap = {
